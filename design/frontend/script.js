8343c2cd4a67ab77d9bacbc00c0f9a89041a3650
ADaggett <Daggetad@oregonstate.edu>
2025-02-12 14:38:46 -0800

    Update script.js

--- design/frontend/script.js
+++ design/frontend/script.js
@@ -1,3 +1,11 @@
   document.getElementById("loginButton").addEventListener("click", function() {
     location.window.href = "Map.HTML";
   });
+
+document.getElementById("menuBack").addEventListener("click", function () {
+    document.getElementById("sideMenu").style.width = "250px";
+});
+
+document.getElementById("closeMenu").addEventListener("click", function () {
+    document.getElementById("sideMenu").style.width = "0";
+});
