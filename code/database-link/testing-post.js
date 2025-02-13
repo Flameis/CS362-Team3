9fdb4e61a37ea869e39fe79a611fe1999b826dec
Flameis <Rocketgunslinger@gmail.com>
2025-02-13 01:22:16 -0800

    Update testing-post.js

--- code/database-link/testing-post.js
+++ code/database-link/testing-post.js
@@ -1,7 +1,20 @@
 // Script to add a new plant
 app.post('/plants', (req, res) => {
     const { species_id, image_id, description, location, season, avg_rating, date_added, date_updated, x_coordinate, y_coordinate } = req.body;
-    const sql = 'INSERT INTO Plants (species_id, image_id, description, location, season, avg_rating, date_added, date_updated, x_coordinate, y_coordinate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const sql = `
+        INSERT INTO Plants (
+            species_id,
+            image_id,
+            description,
+            location,
+            season,
+            avg_rating,
+            date_added,
+            date_updated,
+            x_coordinate,
+            y_coordinate
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
     const params = [species_id, image_id, description, location, season, avg_rating, date_added, date_updated, x_coordinate, y_coordinate];
     db.query(sql, params, (err, result) => {
         if (err) {
@@ -19,7 +32,22 @@ app.post('/plants', (req, res) => {
 // Script to update a plant
 app.put('/plants/:id', (req, res) => {
     const { species_id, image_id, description, location, season, avg_rating, date_added, date_updated, x_coordinate, y_coordinate } = req.body;
-    const sql = 'UPDATE Plants SET species_id = ?, image_id = ?, description = ?, location = ?, season = ?, avg_rating = ?, date_added = ?, date_updated = ?, x_coordinate = ?, y_coordinate = ? WHERE plant_id = ?';
+    const sql = `
+        UPDATE Plants
+        SET
+            species_id = ?,
+            image_id = ?,
+            description = ?,
+            location = ?,
+            season = ?,
+            avg_rating = ?,
+            date_added = ?,
+            date_updated = ?,
+            x_coordinate = ?,
+            y_coordinate = ?
+        WHERE
+            plant_id = ?
+    `;
     const params = [species_id, image_id, description, location, season, avg_rating, date_added, date_updated, x_coordinate, y_coordinate, req.params.id];
     db.query(sql, params, (err, result) => {
         if (err) {
@@ -36,7 +64,10 @@ app.put('/plants/:id', (req, res) => {
 
 // Script to delete a plant
 app.delete('/plants/:id', (req, res) => {
-    const sql = 'DELETE FROM Plants WHERE plant_id = ?';
+    const sql = `
+        DELETE FROM Plants
+        WHERE plant_id = ?
+    `;
     const params = [req.params.id];
     db.query(sql, params, (err, result) => {
         if (err) {
