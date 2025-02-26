f31300544015f4293e7398dcb292bcc34ea5c674
ADaggett <Daggetad@oregonstate.edu>
2025-02-26 09:37:42 -0800

    Update script.jsx

--- design/frontend/script.jsx
+++ design/frontend/script.jsx
@@ -2,10 +2,6 @@
     location.window.href = "Map.HTML";
   });
 
-document.getElementById("menuBack").addEventListener("click", function () {
-    document.getElementById("sideMenu").style.width = "250px";
-});
-
-document.getElementById("closeMenu").addEventListener("click", function () {
-    document.getElementById("sideMenu").style.width = "0";
-});
+  document.getElementById("back").addEventListener("click", function() {
+    location.window.href = "Map.HTML";
+  });
