90f028862654dc0668da09482485387db13e002d
Jake-Thompson <thomjak2@oregonstate.edu>
2025-02-25 17:33:19 -0800

    made sitemap for now so dont have to remmeber page names

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,16 +4,18 @@ import DisplayPlants from "./pages/DisplayPlants";
 import Login from "./pages/Login";
 import Account from "./pages/Account";
 import Register from "./pages/Register";
+import SiteMap from "./pages/SiteMap";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<SiteMap />} />
         <Route path="/display-plants" element={<DisplayPlants />} />
         <Route path="/login" element={<Login />} />
         <Route path="/account" element={<Account />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/sitemap" element={<SiteMap />} />
       </Routes>
     </div>
   );
---
+++ demo/src/pages/SiteMap.js
@@ -0,0 +1,15 @@
+function SiteMap() {
+  return (
+    <div style={{padding:'5px'}}>
+      <p>
+      <a href="/display-plants">/display-plants</a><br />
+      <a href="/login">/login</a><br />
+      <a href="/account">/account</a><br />
+      <a href="/register">/register</a><br />
+      <a href="/sitemap">/sitemap</a><br />
+      </p>
+    </div>
+  );
+}
+
+export default SiteMap;
