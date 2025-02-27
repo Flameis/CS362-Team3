b7de29f07d3812d473622316bca3c653364af88c
Jake-Thompson <thomjak2@oregonstate.edu>
2025-02-27 00:19:18 -0800

    added some smaller sizes of silly-logo used it instead of home button move home button to right to stay away from zoom controls

--- demo/src/pages/SiteMap.js
+++ demo/src/pages/SiteMap.js
@@ -3,13 +3,13 @@ import '../styles/general.css'; // Import the general CSS file
 function SiteMap() {
   return (
     <div style={{padding:'5px'}}>
-      <img src='Logo-with-cricle.png' style={{width:'400px'}}/><br />
+      <img src='silly-logo/Logo-no-circle.png' style={{width:'unset'}}/><br />
       <p style={{display:'unset'}}>
-      <a href="/display-plants">/display-plants</a><br />
+      <a href="/map">/map</a><br />
       <a href="/login">/login</a><br />
       <a href="/account">/account</a><br />
       <a href="/register">/register</a><br />
-      <a href="/map">/map</a><br />
+      <a href="/display-plants">/display-plants</a><br />
       <a href="/sitemap">/sitemap</a><br />
       </p>
     </div>
