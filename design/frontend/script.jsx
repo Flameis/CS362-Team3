eee4d85844a194040341c49367167a136bc93e12
Adison <Daggetad@oregonstate.edu>
2025-03-02 16:13:08 -0800

    Update script.jsx

--- design/frontend/script.jsx
+++ design/frontend/script.jsx
@@ -5,3 +5,12 @@
   document.getElementById("back").addEventListener("click", function() {
     location.window.href = "Map.HTML";
   });
+
+  document.getElementById("logOut").addEventListener("click", function() {
+    //ask user if they want to log out
+        const confirmation = confirm("Are you sure you want to log out?");
+    if (confirmation) {
+      //log out functionality
+        window.location.href = "login.html";
+    }
+  });
