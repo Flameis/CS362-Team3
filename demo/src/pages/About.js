6d2ec0ea37aee2edb3b216e05f4602cbacefe45f
Jake-Thompson <thomjak2@oregonstate.edu>
2025-03-08 03:21:17 -0800

    clarification

--- demo/src/pages/About.js
+++ demo/src/pages/About.js
@@ -5,7 +5,7 @@ function SiteMap() {
     <div className="container">
       <p>
         Image attributions:<br/>
-        logo was edited together by Jake Thompson from:<br/>
+        logo was edited together and modified by Jake Thompson from:<br/>
         <a href="https://www.flaticon.com/free-icon/beaver_427502" title="beaver icon" style={{color:"-webkit-link"}}>Beaver icon created by Freepik - Flaticon</a><br/>
         <a href="https://www.flaticon.com/free-icon/deforestation_9442954" title="deforestation icon" style={{color:"-webkit-link"}}>Deforestation icon created by IconBaandar - Flaticon</a><br/>
 
