01156f8c5be52a725ff637426cf73075a0e60e0a
Flameis <Rocketgunslinger@gmail.com>
2025-03-08 22:06:37 -0800

    Revert "navbar focus"
    This reverts commit 7b57a69077d66b07c0a052f1d1a778c87c9906e2.

--- demo/src/App.js
+++ demo/src/App.js
@@ -7,6 +7,7 @@ import Register from "./pages/Register";
 import SiteMap from "./pages/SiteMap";
 import Map from "./pages/Map";
 import About from "./pages/About";
+import PlantDetails from "./pages/PlantDetails";
 import NavBar from "./components/NavBar";
 import "./App.css";
 
@@ -16,12 +17,13 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<SiteMap />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/display-plants" element={<DisplayPlants />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/sitemap" element={<SiteMap />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/plant/:plantId" element={<PlantDetails />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
