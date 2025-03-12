a2142ebae24f252d8b54ee45589ecda9f6455133
Jake-Thompson <thomjak2@oregonstate.edu>
2025-03-12 00:15:11 -0700

    finally figured out why noone else could run md2html

--- project-docs/md2html/convert-Project-Proposal.cmd
+++ project-docs/md2html/convert-Project-Proposal.cmd
@@ -1,1 +1,3 @@
-node marked/bin/marked -i "../Beaver_Botanica.md" -o "../Beaver_Botanica.html"
+if not exist "node_modules/marked/bin/marked.js" cmd /C "npm install marked"
+echo "running"
+node ./node_modules/marked/bin/marked -i "../Beaver_Botanica.md" -o "../Beaver_Botanica.html"
